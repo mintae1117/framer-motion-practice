9bce5b33be98fe62ebac68fa6097485028011e9b
mintaeKim <peterzzo@naver.com>
2024-03-22 20:39:38 +0900

    first commit

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -2,10 +2,5 @@ import 'styled-components';
 
 
 declare module 'styled-components' {
-  export interface DefaultTheme {
-    textColor: string;
-    bgColor: string;
-    accentColor: string;
-    cardBgColor: string;
-  }
+  export interface DefaultTheme {}
 }
--- src/theme.ts
+++ src/theme.ts
@@ -1,11 +1,7 @@
 import { DefaultTheme } from "styled-components";
 
-export const darkTheme: DefaultTheme = {
-    bgColor: "#2f3640",
-    textColor: "white",
-    accentColor: "#9c88ff",
-    cardBgColor: "transparent",
-};
+
+export const darkTheme: DefaultTheme = {};
 
 export const lightTheme: DefaultTheme = {
     bgColor: "whitesmoke",
